dc74f3cf25ce19bf293df7b2c082eb5214fa084d
elin143 <eveline24si@mahasiswa.pcr.ac.id>
2026-04-28 09:46:29 +0700

    login api + axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import { useState } from "react";
-import Dashboard from "./pages/Dashboard";
+import React, { Suspense } from "react";
+// import Dashboard from "./pages/Dashboard";
 import "./assets/tailwind.css";
 import { Routes, Route } from "react-router-dom";
-import Customers from "./pages/Customer";
-import Orders from "./pages/Orders";
-import NotFound from "./pages/NotFound";
-import Error400 from "./pages/Error400";
-import Error401 from "./pages/Error401";
-import Error403 from "./pages/Error403";
-import MainLayout from "./layouts/MainLayout";
-import AuthLayout from "./layouts/AuthLayout";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Forgot from "./pages/auth/Forgot";
+import Loading from "./components/Loading";
 
 
 function App() {
-  // const [count, setCount] = useState(0);
-
+  const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+  const Orders = React.lazy(() => import("./pages/Orders"))
+  const Customer = React.lazy(() => import("./pages/Customer"))
+  const NotFound = React.lazy(() => import("./pages/NotFound"))
+  const Login = React.lazy(() => import("./pages/auth/Login"))
+  const Register = React.lazy(() => import("./pages/auth/Register"))
+  const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
+  const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+  const Error401 = React.lazy(() => import("./pages/Error401"))
+  const Error400 = React.lazy(() => import("./pages/Error400"))
+  const Error403 = React.lazy(() => import("./pages/Error403"))
+  const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
   return (
-        <div className="p-6">
+    <Suspense fallback={<Loading />}>
           <Routes>
             <Route element={<MainLayout/>}>
               <Route path="*" element={<NotFound />} />
               <Route path="/" element={<Dashboard />} />
               <Route path="/orders" element={<Orders />} />
-              <Route path="/customers" element={<Customers />} />
+              <Route path="/customer" element={<Customer />} />
               <Route path="/error-400" element={<Error400 />} />
               <Route path="/error-401" element={<Error401 />} />
               <Route path="/error-403" element={<Error403 />} />
@@ -37,7 +38,7 @@ function App() {
              <Route path="/forgot" element={<Forgot/>} />
         </Route> 
             </Routes>
-        </div>
+         </Suspense>
   );
 }
 
